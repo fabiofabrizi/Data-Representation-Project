967698971805d1a0ef193d23e9e0bb0b973790b5
Fabio Fabrizi <fabio.fabrizi@gmail.com>
2023-01-12 11:12:13 +0000

    removed config credentials

--- createQuoteTable.py
+++ createQuoteTable.py
@@ -1,14 +1,16 @@
+# import files needed to connect to database
 import mysql.connector
+import dbconfig as cfg
+
 
 db = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="root",
-  #user="datarep",  # this is the user name on my mac
-  #passwd="password" # for my mac
-  database="quote"
+  host = cfg.mysql['host'],
+  user = cfg.mysql['user'],
+  password = cfg.mysql['password']
+  database = "quote"
 )
 
+# Connect to db, enter sql, execute and close connection
 cursor = db.cursor()
 sql="CREATE TABLE quote (id INT AUTO_INCREMENT PRIMARY KEY, quote VARCHAR(250), author VARCHAR(250))"
 
